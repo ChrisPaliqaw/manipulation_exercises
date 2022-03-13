448f49312857aa8bbaed32ef63dd6f1f0c063b41
Chris Paliqaw <christophomos@gmail.com>
2022-03-13 01:23:31 +0000

    Pick and place launch uses script from simple_grasping_tc Version 2 uses modified pick_and_place2.py, which   places one block on top of the other

--- scripts/fetch_joint_plan.py
+++ scripts/fetch_joint_plan.py
@@ -11,11 +11,13 @@ moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
 
 robot = moveit_commander.RobotCommander()
+rospy.loginfo(f"{robot.get_group_names()=}")
 scene = moveit_commander.PlanningSceneInterface()    
 group = moveit_commander.MoveGroupCommander("arm")
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=1)
 
 group_variable_values = group.get_current_joint_values()
+rospy.loginfo(f"{group.get_current_joint_values()=}")
 
 group_variable_values[5] = -1.5
 group.set_joint_value_target(group_variable_values)
